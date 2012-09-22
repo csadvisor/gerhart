a321a5130c90d2afcd39bf3fbbdddb6a9bb0b25d
Course Advisor <advisor@cs.stanford.du>
2012-09-22 12:31:11 -0700

    Dynamically fetching user_ctx

--- models/user_ctx_model.php
+++ models/user_ctx_model.php
@@ -2,18 +2,22 @@
 
 class User_ctx_model extends CI_Model {
 
-    var $id = 13725;
-    var $first_name = 'Joe';
-    var $last_name  = 'Schmoe';
-    #var $role = 'admin';
-    #var $role = 'advisor';
-    var $role = 'advisee';
-
     function __construct()
     {
         parent::__construct();
+        $csid = getenv("WEBAUTH_USER");
+        $csid = 'dan.boneh';
+        $query = $this->db->get_where('people', array('primary_csalias' => $csid), 1);
+        $result = $query->result();
+        $result = $result[0];
+
+        $this->id = $result->id;
+        $this->first_name = $result->nam_friendly;
+        $this->last_name = $result->nam_last;
+
+        $this->role = $this->_getRole($this->id);
     }
-    
+  
     /*
      * get
      *
@@ -69,5 +73,38 @@ class User_ctx_model extends CI_Model {
             break;
       default:
       }
+    }
+
+    private function _getRole($id)
+    {
+        $query = $this->db->get_where('people_relations', array('person_id' => $id));
+        $result = $query->result();
+
+        foreach ($result as $relation)
+          foreach ($relation as $key => $value)
+          {
+            if ($key == 'relation_id')
+              switch ($value)
+              {
+              case 10: # MSCS-ACTIVE
+                return 'advisee';
+              case 1: # ACADEMIC-COUNCIL
+                return 'advisor';
+              case 4: # FACULTY-CONSULTING
+                return 'advisor';
+              case 5: # FACULTY-COURTESY
+                return 'advisor';
+              case 6: # FACULTY-REGULAR
+                return 'advisor';
+              case 30: # FACULTY-VISITING
+                return 'advisor';
+              case 44: # LECTURER
+                return 'advisor';
+              case 79: # FACULTY-EMRITUS
+                return 'advisor';
+              case 3: # STAFF-ADMIN
+                return 'admin';
+              }
+          }
     }
 }
