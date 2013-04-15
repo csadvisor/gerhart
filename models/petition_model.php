ffee327f5434c162daf6f2688fcbcd6e2aa9b4a2
Jack Dubie <jdubie@cs.stanford.edu>
2013-04-15 16:11:04 -0700

    Cleaned up Petition#all

--- models/petition_model.php
+++ models/petition_model.php
@@ -35,26 +35,57 @@ class Petition_model extends CI_Model {
    */
   function all()
   {
-    # double join example:
-    #
-    # SELECT t.PhoneNumber1, t.PhoneNumber2, 
-    #      t2.SomeOtherFieldForPhone1, t3.someOtherFieldForPhone2
-    #      FROM Table1 t
-    #      JOIN Table2 t1 ON t1.PhoneNumber = t.PhoneNumber1
-    #      JOIN Table2 t2 ON t2.PhoneNumber = t.PhoneNumber2
+    /*
+     * double join example:
+     *
+     * SELECT t.PhoneNumber1, t.PhoneNumber2, 
+     *      t2.SomeOtherFieldForPhone1, t3.someOtherFieldForPhone2
+     *      FROM Table1 t
+     *      JOIN Table2 t1 ON t1.PhoneNumber = t.PhoneNumber1
+     *      JOIN Table2 t2 ON t2.PhoneNumber = t.PhoneNumber2
+     */
     
-    $s_fields = 't1.nam_last as s_last, t1.nam_friendly as s_first, t1.primary_csalias as s_alias'; 
-    $s_fields = $s_fields . ', m.ugrad_school as s_school';
-    $a_fields = 't2.nam_last as a_last, t2.nam_friendly as a_first, t2.primary_csalias as a_alias'; 
+    /*
+     * Student fields
+     */
+    $s_fields = array(
+      't1.nam_last as s_last',
+      't1.nam_friendly as s_first',
+      't1.primary_csalias as s_alias',
+      'm.ugrad_school as s_school',
+    );
+
+    /*
+     * Advisor fields
+     */
+    $a_fields = array(
+      't2.nam_last as a_last',
+      't2.nam_friendly as a_first',
+      't2.primary_csalias as a_alias',
+    );
+
+    /*
+     * concat fields into comma separated array
+     */
+    $s_fields = implode(', ', $s_fields);
+    $a_fields = implode(', ', $a_fields);
+
+    /*
+     * Grab fields from petitions and advisee and advisor
+     */
     $this->db->select('t.*, ' . $a_fields . ', ' . $s_fields); 
     $this->db->join('people t1', 't1.id = t.student_id'); 
     $this->db->join('people t2', 't2.id = t.advisor_id'); 
     $this->db->join('mscsactive m', 'm.person_id = t.student_id');
 
-    # sort by student last name
+    /*
+     * sort by student last name
+     */
     $this->db->order_by('s_last');
     
-    # select records relevant to user 
+    /*
+     * select records relevant to user 
+     */
     $criteria = $this->User_ctx_model->addRoleFKey(array(), 't.'); 
     $query = $this->db->get_where($this->TABLE_NAME . ' t', $criteria); 
     
